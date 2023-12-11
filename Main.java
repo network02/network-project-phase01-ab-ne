932ff4fe9ed7037ee8b189e5115ad56aba2dd6e9
Nasrin-esh <nsrynashaqyan@gmail.com>
2023-12-11 21:53:01 +0330

    get and check if host is online

--- Main.java
+++ Main.java
@@ -1,43 +1,170 @@
-import java.io.IOException;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        MainPage();
+    }
 
-        System.out.println("1-check if a host is online");
-        System.out.println("2-check ports and print services");
-        System.out.println("3-get method");
-        System.out.println("4-post method");
+    public static void MainPage() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------------------------------------------------");
+        System.out.println("                  Welcome To Nmap                  ");
+        System.out.println("---------------------------------------------------");
+        System.out.println("    1 - Check Host Status                          ");
+        System.out.println("    2 - Check Port Status of a Host                ");
+        System.out.println("    3 - Get                                  ");
+        System.out.println("    4 - Post                                 ");
+        System.out.println("---------------------------------------------------");
+        System.out.print("Choose a number : ");
+        int inputNumMainPage = scanner.nextInt();
 
-        Scanner sc = new Scanner(System.in);
+        switch (inputNumMainPage) {
+            case 1:
+                isOnline();
+                break;
+            case 2:
+                checkinhPorys();
+                break;
+            case 3:
+                get();
+                break;
+            case 4:
+                break;
+            default:
+                MainPage();
+        }
+    }
 
-        int choice = sc.nextInt();
+    private static String getServiceName(int port) { // تابع برای گرفتن نام سرویس بر اساس شماره پورت
+        switch (port) {
+            case 20:
+                return "FTP Data Transfer";
+            case 21:
+                return "FTP Control";
+            case 22:
+                return "SSH";
+            case 23:
+                return "Telnet";
+            case 25:
+                return "SMTP";
+            case 53:
+                return "DNS";
+            case 80:
+                return "HTTP";
+            case 443:
+                return "HTTPS";
+            default:
+                return "Unknown Service";
+        }
+    }
 
-        Request request = new Request();
+    private static void checkinhPorys(){
+        Scanner scanner = new Scanner(System.in);
+        // دریافت هاست و رنج پورت‌ها از کاربر
+        System.out.print("Enter the host/IP address: ");
+        String host = scanner.nextLine();
+        System.out.print("Enter the starting port: ");
+        int startPort = scanner.nextInt();
+        System.out.print("Enter the ending port: ");
+        int endPort = scanner.nextInt();
 
-        /*
-        if (choice == 1)
-        {
-            System.out.println("enter IP address of host");
+        System.out.println("Scanning ports...");
 
-            String IP = sc.next();
-            int port = sc.nextInt();
+        // اسکن پورت‌ها
+        for (int port = startPort; port <= endPort; port++) {
+            try {
+                // ایجاد یک سوکت با تاخیر زمانی 100 میلی‌ثانیه برای هر پورت
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(host, port), 100);
 
-            if (request.isOnline(IP,port) )
-                System.out.println( IP + " is online");
-            else
-                System.out.println(IP + " is offline");
+                // نمایش اطلاعات پورت باز
+                if(getServiceName(port)!= "Unknown Service")
+                    System.out.println("Port " + port + " is open - Service: " + getServiceName(port));
+
+                // بستن سوکت
+                socket.close();
+            } catch (Exception e) {
+                // پورت بسته است یا خطای دیگر
+            }
         }
 
-        else if (choice == 2)
-            request.checkPorts();
-        else if (choice == 3)
-            request.get();
-        else
-            request.post();
+        // بستن اسکنر
+        scanner.close();
+    }
 
-        String ip = sc.next();
+
+    private static boolean isOnline(String host ,int port) throws IOException {
+        Socket s = new Socket(host,port);
+        return s.isConnected();
+
+        /*
+        try(Socket socket = new Socket()) {
+            s.connect(new InetSocketAddress(host,port),2000);
+            System.out.println("online");
+        }
+        catch (Exception e)
+        {
+            System.out.println("offline");
+        }
 
          */
+
+
+
+
+
+
     }
+
+    private static void isOnline() throws IOException {
+        System.out.println("enter the host/IP address");
+
+        Scanner scanner = new Scanner(System.in);
+        String host = scanner.next();
+
+        System.out.println("enter the post number");
+        int port = scanner.nextInt();
+
+        if (isOnline(host,port))
+            System.out.println("HOST IS ONLINE");
+        else
+            System.out.println("HOST IS OFFLINE");
+    }
+
+    private static void get() throws IOException {
+        String host = "localhost";
+        int port = 8080;
+        try {
+            // Create a socket and connect to the server
+            Socket socket = new Socket(host, port);
+
+            // Send a GET request for a user with ID 1
+            String request = "GET /users/1\r\n";
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            writer.write(request);
+            writer.flush();
+
+            // Read the server's response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String responseLine;
+            while ((responseLine = reader.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+
+            // Close the connections
+            reader.close();
+            writer.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+
 }
+
+
--- Request.java
+++ Request.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Request {
 
@@ -7,28 +11,58 @@ public class Request {
 
 
 
-    /*
-    public boolean isOnline(String IPAddress, int portNumber) throws IOException {
-
-        socket = new Socket(IPAddress,portNumber);
-
-        if (socket.isConnected())
-            return true;
-        else return false;
-
-    }
 
 
-    void checkPorts()
-    {
-        socket = new Socket();
-        socket.connect();
 
-    }
 
-    void get(){}
+        public void SendReq() throws Exception
+        {
 
-    void post(){}
+            //Instantiate a new socket
+            Socket s = new Socket("localhost", 8080);
 
-     */
+            //Instantiates a new PrintWriter passing in the sockets output stream
+            PrintWriter wtr = new PrintWriter(s.getOutputStream());
+
+            //Prints the request string to the output stream
+            wtr.println("GET / HTTP/1.1");
+            wtr.println("Host: www.badunetworks.com");
+            wtr.println("");
+            wtr.flush();
+
+            //Creates a BufferedReader that contains the server response
+            BufferedReader bufRead = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String outStr;
+
+            //Prints each line of the response
+            while((outStr = bufRead.readLine()) != null){
+                System.out.println(outStr);
+            }
+
+
+            //Closes out buffer and writer
+            bufRead.close();
+            wtr.close();
+
+        }
+
+        void get()  {
+
+            try
+            {
+                Socket s = new Socket("localhost ",8080);
+                //s.isConnected();
+            }
+
+
+             catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
 }
